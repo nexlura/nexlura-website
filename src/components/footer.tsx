7d151c2b00d63487822e30a1cc534c4952e97bac
Afiz Savage <savageafiz9@gmail.com>
2025-02-22 11:16:00 +0000

    update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -14,12 +14,11 @@ function CallToAction() {
         <p className="mt-6 text-3xl font-medium tracking-tight text-gray-950 sm:text-5xl">
           Ready to dive in?
           <br />
-          Start your free trial today.
+          Get a free consultion today.
         </p>
       </hgroup>
       <p className="mx-auto mt-6 max-w-xs text-sm/6 text-gray-500">
-        Get the cheat codes for selling and unlock your team&apos;s revenue
-        potential.
+        Leverage cutting-edge technology to streamline operations, and drive business growth.
       </p>
       <div className="mt-6">
         <Button className="w-full sm:w-auto" href="#">
@@ -53,33 +52,25 @@ function Sitemap() {
   return (
     <>
       <div>
-        <SitemapHeading>Product</SitemapHeading>
-        <SitemapLinks>
-          <SitemapLink href="/pricing">Pricing</SitemapLink>
-          <SitemapLink href="#">Analysis</SitemapLink>
-          <SitemapLink href="#">API</SitemapLink>
-        </SitemapLinks>
-      </div>
-      <div>
         <SitemapHeading>Company</SitemapHeading>
         <SitemapLinks>
-          <SitemapLink href="#">Careers</SitemapLink>
-          <SitemapLink href="/blog">Blog</SitemapLink>
-          <SitemapLink href="/company">Company</SitemapLink>
+          <SitemapLink href="#">About</SitemapLink>
+          <SitemapLink href="/blog">Process</SitemapLink>
+          <SitemapLink href="/company">Contact Us</SitemapLink>
         </SitemapLinks>
       </div>
       <div>
-        <SitemapHeading>Support</SitemapHeading>
+        <SitemapHeading>Connect</SitemapHeading>
         <SitemapLinks>
-          <SitemapLink href="#">Help center</SitemapLink>
-          <SitemapLink href="#">Community</SitemapLink>
+          <SitemapLink href="#">Facebook</SitemapLink>
+          <SitemapLink href="#">Twitter</SitemapLink>
+          <SitemapLink href="#">Instagram</SitemapLink>
         </SitemapLinks>
       </div>
       <div>
-        <SitemapHeading>Company</SitemapHeading>
+        <SitemapHeading>Work</SitemapHeading>
         <SitemapLinks>
-          <SitemapLink href="#">Terms of service</SitemapLink>
-          <SitemapLink href="#">Privacy policy</SitemapLink>
+          <SitemapLink href="/pricing">Propati</SitemapLink>
         </SitemapLinks>
       </div>
     </>
@@ -148,7 +139,7 @@ function SocialLinks() {
 function Copyright() {
   return (
     <div className="text-sm/6 text-gray-950">
-      &copy; {new Date().getFullYear()} Radiant Inc.
+      &copy; {new Date().getFullYear()} Nexlura LLC.
     </div>
   )
 }
