88dc6882a5a5d9b3b9b084bf5cfbbf4c132d78e2
Afiz Savage <savageafiz9@gmail.com>
2025-02-26 07:42:33 +0000

    add social media icons

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -102,39 +102,51 @@ function ContactDetails() {
     return (
         <FadeIn>
             <h2 className="font-display text-base font-semibold text-neutral-950">
-                Our offices
+                Office Address
             </h2>
             <p className="mt-6 text-base text-neutral-600">
-                Prefer doing things in person? We don’t but we have to list our
-                addresses here for legal reasons.
+                Get in touch with us—visit our office, drop an email, give us a call, or connect with us on social media.
             </p>
-
             <Offices className="mt-10 grid grid-cols-1 gap-8 sm:grid-cols-2" />
 
             <Border className="mt-16 pt-16">
                 <h2 className="font-display text-base font-semibold text-neutral-950">
-                    Email us
+                    Contact Details
                 </h2>
                 <dl className="mt-6 grid grid-cols-1 gap-8 text-sm sm:grid-cols-2">
-                    {[
-                        ['Careers', 'careers@studioagency.com'],
-                        ['Press', 'press@studioagency.com'],
-                    ].map(([label, email]) => (
-                        <div key={email}>
-                            <dt className="font-semibold text-neutral-950">{label}</dt>
-                            <dd>
-                                <Link
-                                    href={`mailto:${email}`}
-                                    className="text-neutral-600 hover:text-neutral-950"
-                                >
-                                    {email}
-                                </Link>
-                            </dd>
-                        </div>
-                    ))}
+
+                    <div >
+                        <dt className="font-semibold text-neutral-950">Phone</dt>
+                        <dd>
+                            <Link
+                                href={`tel:+232 79 712365`}
+                                className="text-neutral-600 hover:text-neutral-950"
+                            >
+                                +232 79 712365
+                            </Link>
+                        </dd>
+                        <dd>
+                            <Link
+                                href={`tel:+232 30 847661`}
+                                className="text-neutral-600 hover:text-neutral-950"
+                            >
+                                +232 30 847661
+                            </Link>
+                        </dd>
+                    </div>
+                    <div>
+                        <dt className="font-semibold text-neutral-950">Email</dt>
+                        <dd>
+                            <Link
+                                href={`mailto:nexlura@gmail.com`}
+                                className="text-neutral-600 hover:text-neutral-950"
+                            >
+                                nexlura@gmail.com
+                            </Link>
+                        </dd>
+                    </div>
                 </dl>
             </Border>
-
             <Border className="mt-16 pt-16">
                 <h2 className="font-display text-base font-semibold text-neutral-950">
                     Follow us
